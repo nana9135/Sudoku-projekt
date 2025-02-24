dfbfeae55d6e5744606102251b6e834df10a9427
hada6610 <hayk.davtyan.6610@student.uu.se>
2025-02-24 21:55:05 +0100

    Update sudoku.ts

--- sudoku.ts
+++ sudoku.ts
@@ -24,3 +24,15 @@ function is_valid(board, row, col, num){
 function solve(board){
     return false;
 }
+
+function contains_zero(board){
+    for (let row = 0; row < 9; row++){
+        for(let col = 0; col < 9; col++){
+            if (board[row][col] === 0){
+                return [row,col]; }
+            }
+        }
+        return null;
+    }
+ 
+    
